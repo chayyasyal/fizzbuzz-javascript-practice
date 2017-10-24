be719e4e5cb9ef5a7f2321945ce07f6ede1cd237
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-24 17:21:16 +0100

    Passing code and tests used in FizzBuzz exercise using JavaScript

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,10 @@
+describe('FizzBuzz', function(){
+  var fizzbuzz;
+  describe('knows when a number is NOT', function(){
+    it('divisible by 3', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+});
